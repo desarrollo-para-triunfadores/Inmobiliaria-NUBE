d8b840b98f5c9cb1b8802ca2e89627eca51ba2c1
Horacio Alejandro Kuszniruk <hacho.kuszniruk@gmail.com>
2018-07-10 18:00:35 -0300

    corrección en la clase de inmuebles para una function del front

--- NUBE/app/Inmueble.php
+++ NUBE/app/Inmueble.php
@@ -110,7 +110,7 @@ class Inmueble extends Model {
     }
 
     public function foto_slider() {
-        return $this->fotos->where('seccion_imagen', 'slider')->get()->first();
+        return $this->fotos->where('seccion_imagen', 'slider')->last();//->get()->first();
     }
 
 }
